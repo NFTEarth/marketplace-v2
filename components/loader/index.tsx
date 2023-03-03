2f1e3e9503d6dbe355dffd1021cbdaf36730f2fc
shawnkeqin <e0052482@u.nus.edu>
2023-03-03 10:01:29 +0800

    chore: add types

--- components/loader/index.tsx
+++ components/loader/index.tsx
@@ -2,7 +2,12 @@ import { Box } from 'components/primitives'
 import { useState, useEffect } from 'react'
 import { ProgressDiv, Progress } from './styled'
 
-export const Loader = ({ width, percent }) => {
+type Props = {
+  width: number
+  percent: number
+}
+
+export const Loader = ({ width, percent }: Props) => {
   const [value, setValue] = useState(0)
 
   useEffect(() => {
